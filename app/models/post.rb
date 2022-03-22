5927be60c87ddc221c6b071afd9e6c1d5cda60c0
ideed <emanuelivan45@gmail.com>
2022-03-22 18:47:03 +0000

    Updated header, added filters for when user is logged in/out

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
   has_many :comments
+  after_save :touch_comments
+
+  def touch_comments
+    comments.each(&:touch)
+  end
 end
