a0b430000ab4b45e143d443769d47db4385c3ab6
ideed <emanuelivan45@gmail.com>
2022-03-26 13:17:58 +0000

    Added Singleton and Decorator Design Patterns

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    @tweets = getTwitterAPI
+    api = GetTwitterAPI.new(Apis.new("twitter.com"))
+    @tweets = api.returnApi
     @posts = Post.all
     if session[:yourPost_ids]
       @yourPosts = Array.new
@@ -13,10 +14,10 @@ class PostsController < ApplicationController
   end
 
   def show
-    irish_news = IrishNewsAPI.new
+    irish_news = IrishNewsAPI.new(Apis.new("newsapi.org"))
     index = 0
     articles = Array.new
-    irish_news.posts.each do |post|
+    irish_news.returnApi.each do |post|
       if index == 2
          post[1].each do |article|
            articles.push(article)
@@ -29,10 +30,8 @@ class PostsController < ApplicationController
   end
 
   def new
-    time = Time.new
-    values = time.to_a
-    sendDate = "#{Time.utc(*values)}"
-    @post = Post.new(up_votes: 0, down_votes: 0, send_date:sendDate)
+    local_time = LocalTime.instance
+    @post = Post.new(up_votes: 0, down_votes: 0, send_date:local_time.calculateTime)
   end
 
   def edit
@@ -78,27 +77,66 @@ class PostsController < ApplicationController
     end
   end
 
-  def getTwitterAPI
-    @consumer_key = "62ipBXBp2j46sfc7o24ObDRhR"
-    @consumer_secret = "XBfih8LSbV4HZmNfhQArIc2lLnGj298YHADfClnA7YRveOiJn1"
-    @access_token = "2279402058-MjlA8FMoAOtjdFUAnleb0tjM4IoyUCGgFclGeLI"
-    @access_secret = "1BJ06K2Kd6Yicw0MyPX1YkD50BSZsWEjKb1jgc8gsJu8Y"
+  # Design Pattern Implementations
+  class LocalTime
+    include Singleton
 
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = @consumer_key
-      config.consumer_secret     = @consumer_secret
-      config.access_token        = @access_token
-      config.access_token_secret = @access_secret
+    def calculateTime
+      time = Time.new
+      values = time.to_a
+      @sendDate = "#{Time.utc(*values)}"
     end
-
-    client.user_timeline('Education_Ire', count: 6)
   end
 
-  class IrishNewsAPI
-    include HTTParty
-    base_uri "newsapi.org/"
+  class Apis
+    def initialize(api_url)
+      @api_url = api_url
+    end
+    def returnApi
+      puts("Api request sent to URL: #{@api_url}")
+    end
+  end
 
-    def posts
+  class ApiDecorator
+    def initialize(real_api)
+      @real_api = real_api
+    end
+    def returnApi
+      @real_api.returnApi
+    end
+  end
+
+  class GetTwitterAPI < ApiDecorator
+    def initialize(api)
+      super(api)
+      @consumer_key = "62ipBXBp2j46sfc7o24ObDRhR"
+      @consumer_secret = "XBfih8LSbV4HZmNfhQArIc2lLnGj298YHADfClnA7YRveOiJn1"
+      @access_token = "2279402058-MjlA8FMoAOtjdFUAnleb0tjM4IoyUCGgFclGeLI"
+      @access_secret = "1BJ06K2Kd6Yicw0MyPX1YkD50BSZsWEjKb1jgc8gsJu8Y"
+    end
+
+    def returnApi
+      super
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = @consumer_key
+        config.consumer_secret     = @consumer_secret
+        config.access_token        = @access_token
+        config.access_token_secret = @access_secret
+      end
+
+      client.user_timeline('Education_Ire', count: 6)
+    end
+  end
+
+  class IrishNewsAPI < ApiDecorator
+    include HTTParty
+    def initialize(api)
+      super(api)
+      self.class.base_uri "newsapi.org/"
+    end
+
+    def returnApi
+      super
       self.class.get('/v2/top-headlines?apiKey=8346750eaa5c4a1393c80988f56a7521&country=ie')
     end
   end
